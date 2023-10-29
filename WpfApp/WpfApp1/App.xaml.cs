d51769139840e1c36cd17902c045a9c0a4a87e7a
Kristina Kiseleva <toma.toma.tomat1061@gmail.com>
2023-10-29 23:05:55 +0300

    Creating WPF Applications
    Creating WPF Applications
    Kristina Kiseleva ISP 313

---
+++ WpfApp/WpfApp1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+           this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            Window0 win = new Window0();
+            win.Title = "Перекрывающий заголовок окна";
+            win.Show();
+        }
+    }
+}
---
+++ WpfApp/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class Window0 : Window
+    {
+        System.Windows.Media.Brush color;
+        bool colorFlag = true;
+
+        public Window0()
+        {
+            //InitializeComponent();
+            Application.LoadComponent(this, new Uri("Window0.xaml", UriKind.Relative));
+
+            Btn1.Click += new RoutedEventHandler(Btn1_Click);
+            color = this.Window.Background;
+        }
+
+        void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (colorFlag)
+                this.Window.Background = System.Windows.Media.Brushes.Purple;
+            else
+                this.Window.Background = color;
+
+            colorFlag = !colorFlag;
+        }
+    }
+}
